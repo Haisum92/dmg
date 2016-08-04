44d7dd20ef71c651d8cb067785291566ffc45609
Haisum92 <usman.haisum@gmail.com>
2016-08-05 01:21:04 +0500

    dbconfig updated

--- config/dbconfig.php
+++ config/dbconfig.php
@@ -1,14 +1,17 @@
 <?php
 $db_prefix = "portal";
 $config = array (
+		'db_users'           => $db_prefix.'_users',
 		'db_areas'           => $db_prefix.'_areas',
+		'db_user_privileges' => $db_prefix.'_user_privileges',
 		'db_branches'        => $db_prefix.'_branches',
+		'db_area_branches'   => $db_prefix.'_area_branches',
 		'db_menus'           => $db_prefix.'_menus',
 		'db_branch_menus'    => $db_prefix.'_branch_menus',
 		'db_menu_sections'   => $db_prefix.'_menu_sections',
 		'db_sections'        => $db_prefix.'_sections',
-		'db_user_privileges' => $db_prefix.'_user_privileges',
-		'db_users'           => $db_prefix.'_users',
+		'db_posts'           => $db_prefix.'_posts',
+		'db_post_images'     => $db_prefix.'_post_images',
 		'db_login_details'   => $db_prefix.'_login_details',
     );
 
